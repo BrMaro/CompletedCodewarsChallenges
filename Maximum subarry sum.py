aac92bf099033d556f7f9a73e20105f38b8c2496
BrMaro <keremamariari@gmail.com>
2023-10-04 15:56:16 +0300

    origin #2

--- Maximum subarry sum.py
+++ Maximum subarry sum.py
@@ -1,28 +1,11 @@
-def all_negative(arr):
-    for element in arr:
-        if element >= 0:
-            return False
-    return True
 
-def max_sequence(arr):
-    if len(arr)>0 and not all_negative(arr):
-        max_sum = arr[0]
-        for j in range(len(arr)-1):
-            current_sum = arr[j]
-            for i in range(j+1,len(arr)):
-                current_sum = current_sum + arr[i]
-                #print(current_sum,max_sum)
-                if current_sum>max_sum:
-                    max_sum = current_sum
-                    subarray = [arr[j] for j in range(j,i+1)]
-        #print(subarray)
-        return max_sum
-    else:
-        return 0
+"""The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
 
-print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+# should be 6: [4, -1, 2, 1]
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
 
-######KADANES ALGORITHM
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray"""
 def all_negative(arr):
     for element in arr:
         if element >= 0:
