508ecc741d1459372bbd7bd357c4093011ef7ff3
BrMaro <70138725+BrMaro@users.noreply.github.com>
2024-07-30 11:43:13 +0300

    More challenges

---
+++ Python/Scramblies.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+def scramble(s1, s2):
+    count_s1 = Counter(s1)
+    count_s2 = Counter(s2)
+    for char in count_s2:
+        if count_s2[char] > count_s1.get(char, 0):
+            return False
+    return True
+
+print(scramble())
